12c25433071422b27d35c9b0ed6a9992031be51c
JDM Ubuntu VB <JKDMarks@gmail.com>
2019-04-02 12:25:47 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,9 @@
 def count_elements(array)
-  # code goes here
+  array.each_with_object({}) do |elt, new_hash|
+    if !new_hash[elt]
+      new_hash[elt] = 1
+    else
+      new_hash[elt] += 1
+    end
+  end
 end
- 
